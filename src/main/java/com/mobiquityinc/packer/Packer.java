a7b6d34cc6f8cb76029717ca67b958c16f10c9ee
ent_gcaseli <ent_gcaseli@uolinc.com>
2019-09-09 11:43:44 -0300

    test file path

--- src/main/java/com/mobiquityinc/packer/Packer.java
+++ src/main/java/com/mobiquityinc/packer/Packer.java
@@ -1,18 +1,44 @@
 package com.mobiquityinc.packer;
 
 import com.mobiquityinc.exception.APIException;
+import com.sun.xml.internal.ws.util.StringUtils;
+import java.io.File;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
 
 public class Packer {
 
-  private Packer() {
-  }
+    private Packer() {
+    }
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
 
-  }
+    }
 
-  public static String pack(String filePath) throws APIException {
-    return null;
-  }
+    public static String pack(String filePath) throws APIException {
+
+        if (isValidPath(filePath)){
+            return "";
+        }
+        return "";
+    }
+
+
+    public static boolean isValidPath(String path) throws APIException {
+        try {
+            if(path.isEmpty()){
+                throw new APIException("Empty path:" + path);
+            }
+
+            File file = new File(path);
+            if(!file.exists()){
+                throw new APIException("File does not exists in path:" + path);
+            }
+        } catch (InvalidPathException | NullPointerException ex) {
+            throw new APIException("Error when read path:" + path, ex);
+        }
+        return true;
+    }
+
 }
--- src/test/java/com/mobiquityinc/packer/PackerTest.java
+++ src/test/java/com/mobiquityinc/packer/PackerTest.java
@@ -2,11 +2,18 @@ package com.mobiquityinc.packer;
 
 import static org.junit.Assert.*;
 
+import com.mobiquityinc.exception.APIException;
 import org.junit.Test;
 
 public class PackerTest {
 
-    @Test
-    public void pack() {
+    @Test(expected = APIException.class)
+    public void shouldThrownAnExceptionEmptyPath() throws APIException {
+        Packer.pack("");
+    }
+
+    @Test(expected = APIException.class)
+    public void shouldThrownAnExceptionFileNotExists() throws APIException {
+        Packer.pack("/testfile.txt");
     }
 }
