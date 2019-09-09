438d183db72b084aee4356edfe1676a87db14b9a
ent_gcaseli <ent_gcaseli@uolinc.com>
2019-09-09 09:55:12 -0300

    project structure

---
+++ src/main/java/com/mobiquityinc/packer/Packer.java
@@ -0,0 +1,18 @@
+package com.mobiquityinc.packer;
+
+import com.mobiquityinc.exception.APIException;
+
+public class Packer {
+
+  private Packer() {
+  }
+
+  public static void main(String[] args) {
+
+
+  }
+
+  public static String pack(String filePath) throws APIException {
+    return null;
+  }
+}
